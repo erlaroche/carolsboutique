97d081b0549db02bd4e2bb2cdc047930dc1f193a
Emma Laroche <E.R.Laroche@gmail.com>
2015-05-16 19:10:45 -0500

    Add path for routes, add foundation navagation top-bar with a fixed class.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/gallary'
+  get 'welcome/index', :as => 'home'
+  get 'welcome/gallary', :as => 'gallary'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
